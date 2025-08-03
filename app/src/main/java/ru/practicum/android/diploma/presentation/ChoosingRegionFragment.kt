17c5159fc97cd383a064c3440bb1d23528bda61f
Daniil <dan07102000@gmail.com>
2025-08-03 19:12:13 +0300

    Создал граф навигации и заглушки для всех экранов приложения

---
+++ app/src/main/java/ru/practicum/android/diploma/presentation/ChoosingRegionFragment.kt
@@ -0,0 +1,23 @@
+package ru.practicum.android.diploma.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ru.practicum.android.diploma.databinding.FragmentChoosingRegionBinding
+
+class ChoosingRegionFragment : Fragment() {
+
+    private var _binding: FragmentChoosingRegionBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentChoosingRegionBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
