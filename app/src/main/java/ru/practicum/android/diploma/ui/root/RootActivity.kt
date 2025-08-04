c4132687e0017eeadcaace82f03bfb292bb35ebf
Daniil <dan-07102000@mail.ru>
2025-08-04 16:29:11 +0300

    Реализовал и настроил BottomNavigationView, а также всем фрагментам задал белый цвет фона

--- app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
@@ -5,6 +5,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.ActivityRootBinding
 
 class RootActivity : AppCompatActivity() {
@@ -29,6 +32,12 @@ class RootActivity : AppCompatActivity() {
             )
             insets
         }
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.root_fragment_container_view) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bottomNavView.setupWithNavController(navController)
 
     }
 
