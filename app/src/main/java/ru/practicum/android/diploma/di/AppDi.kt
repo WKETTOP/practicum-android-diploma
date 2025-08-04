86562e4a0f3aebca67cbd4a7d1c07660a8c8d5aa
HALOPENE <jebory@mail.ru>
2025-08-03 23:36:36 -0400

    Добавление БД

--- app/src/main/java/ru/practicum/android/diploma/di/AppDi.kt
+++
@@ -1,6 +0,0 @@
-package ru.practicum.android.diploma.di
-import org.koin.dsl.module
-
-val appModule = module {
-//    Для SharedPreferences, Room и тд.
-}
--- app/src/main/java/ru/practicum/android/diploma/util/App.kt
+++ app/src/main/java/ru/practicum/android/diploma/util/App.kt
@@ -3,9 +3,9 @@ package ru.practicum.android.diploma.util
 import android.app.Application
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.GlobalContext.startKoin
-import ru.practicum.android.diploma.di.appModule
 import ru.practicum.android.diploma.di.dataModule
-import ru.practicum.android.diploma.di.domainModule
+import ru.practicum.android.diploma.di.interactorModule
+import ru.practicum.android.diploma.di.repositoryModule
 import ru.practicum.android.diploma.di.viewModelModule
 
 class App : Application() {
@@ -14,7 +14,7 @@ class App : Application() {
         super.onCreate()
         startKoin {
             androidContext(this@App)
-            modules(listOf(appModule, dataModule, domainModule, viewModelModule))
+            modules(listOf(dataModule, interactorModule, repositoryModule, viewModelModule))
         }
     }
 }
